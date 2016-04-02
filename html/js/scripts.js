eeb57b12f85dd7fb39fd8be29ab6e98071574654
malenalopez5 <malenalopez5@gmail.com>
2016-04-02 10:29:18 -0400

    committing amelia

---
+++ html/js/scripts.js
@@ -0,0 +1,38 @@
+$( document ).ready(function() {
+
+    $('.filters .toggle').click(function(){
+        $(this).toggleClass('open');
+        $('.filters .options').fadeToggle();
+    });
+
+    $('.dropdown label').click(function(){
+        $(this).parent().parent().find('label').removeClass('active');
+        $(this).toggleClass('active');
+    });
+
+
+    /* Show/hide user menu */
+    $('.user h4').click(function(){
+        $('.user-menu').fadeToggle('fast');
+        $('.user h4').toggleClass('over');
+    });
+
+    $('.user').mouseleave(function(){
+        $('.user-menu').fadeOut('fast');
+        $('.user h4').removeClass('over');
+    });
+
+    /* Remove tags from filters */
+    $('div.tags li a').click(function(){
+        $(this).parent().fadeOut();
+    });
+
+
+    /* Accordion */
+   //$('.panel-title').click(function(){
+    //   $('.panel-heading').removeClass('open');
+    //   $(this).parents('.panel-heading').addClass('open');
+    //});
+
+  
+});
