7cce44f0cfc910837c22cc4c7498f784a62fac18
malenalopez5 <malenalopez5@gmail.com>
2016-04-02 14:37:17 -0400

    map

--- js/map.js
+++ js/map.js
@@ -1,7 +1,3 @@
-$( document ).ready(function() {
-    console.log( "ready!" );
-
-	L.mapbox.accessToken = 'pk.eyJ1IjoibWFsZW5hbG9wZXo1IiwiYSI6ImNpbHBqYzBrMTA4ZnB0c20xemYzM2F2azUifQ.HqfiPXTJxF4hXa1ktcURNw';
-  	L.mapbox.map('map-one', 'malenalopez5.pi5gln11').setView([38.8929,-77.0252], 14);
-
-  });
+L.mapbox.accessToken = 'pk.eyJ1IjoibWFsZW5hbG9wZXo1IiwiYSI6ImNpbHBqYzBrMTA4ZnB0c20xemYzM2F2azUifQ.HqfiPXTJxF4hXa1ktcURNw';
+				var map = L.mapbox.map('map','malenalopez5.pi5gln11')
+   				 .setView([38.91, -77.101], 12);
